b0451eb14ba00395f69d09dff6447e05d38b3e7e
Ayman Barghout <59527019+Ayman-Kortobaa@users.noreply.github.com>
2020-11-23 17:32:47 +0200

    Reverted aes_gcm.dart to base64 encoding.

--- lib/aes_gcm.dart
+++ lib/aes_gcm.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:pc_steelcrypt/pointycastle.dart';
+import 'package:steel_crypt/PointyCastleN/export.dart';
 
 class AesGcm {
   Uint8List _key32;
@@ -12,8 +13,8 @@ class AesGcm {
         ParametersWithIV<KeyParameter>(KeyParameter(_key32), iv), null);
     PaddedBlockCipher cipher = PaddedBlockCipher('AES/GCM/PKCS7');
     cipher..init(true, params);
-    Uint8List inter = cipher.process(Uint8List.fromList(input.codeUnits));
-    return String.fromCharCodes(inter);
+    Uint8List inter = cipher.process(utf8.encode(input) as Uint8List);
+    return base64.encode(inter);
   }
 
   String decrypt(String encrypted, [Uint8List iv]) {
@@ -21,7 +22,7 @@ class AesGcm {
         ParametersWithIV(KeyParameter(_key32), iv), null);
     PaddedBlockCipher cipher = PaddedBlockCipher('AES/GCM/PKCS7');
     cipher..init(false, params);
-    Uint8List inter = cipher.process(Uint8List.fromList(encrypted.codeUnits));
-    return String.fromCharCodes(inter);
+    Uint8List inter = cipher.process(base64.decode(encrypted));
+    return utf8.decode(inter);
   }
 }
